9cdcdf3719b99fa6da3eaa26021d369247d31397
Howler Nofelle <hilmy@wahidin.net>
2022-04-03 00:11:20 +0700

    add list hotel activity

---
+++ app/src/main/java/com/barney/dmcprojectprayge/ListHotelActivity.java
@@ -0,0 +1,14 @@
+package com.barney.dmcprojectprayge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListHotelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_hotel);
+    }
+}
