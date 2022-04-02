9e4d96c264554d90502aa8dc0d40c5cd577c3ca5
Howler Nofelle <hilmy@wahidin.net>
2022-04-03 00:17:17 +0700

    add list restaurant activity

---
+++ app/src/main/java/com/barney/dmcprojectprayge/ListRestaurantActivity.java
@@ -0,0 +1,14 @@
+package com.barney.dmcprojectprayge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListRestaurantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_restaurant);
+    }
+}
