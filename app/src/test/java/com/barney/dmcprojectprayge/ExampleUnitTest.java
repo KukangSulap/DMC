5c2f9f12209ad4ed5c2c61340a35bc5878a4da97
Howler Nofelle <hilmy@wahidin.net>
2022-07-05 11:44:25 +0700

    added radius for list using cardview library

--- app/src/test/java/com/barney/dmcprojectprayge/ExampleUnitTest.java
+++ app/src/test/java/com/barney/dmcprojectprayge/ExampleUnitTest.java
@@ -1,8 +1,13 @@
 package com.barney.dmcprojectprayge;
 
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
+import android.content.Intent;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
